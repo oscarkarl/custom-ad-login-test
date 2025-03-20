a6cad2a09ec3514f61dc40a210798b1387a920da
Oscar Karlsson <oscar.karlsson@bontouch.com>
2025-03-20 08:56:32 +0100

    Add some javascript, see what we're allowed to do

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("HELLO WORLD");
+
+const style = document.createElement("style");
+style.innerHTML = `
+  @keyframes colorShift {
+    0% { background-color: #333; color: #efefef; }
+    25% { background-color: #aaa; color: #333; }
+    50% { background-color: #ddd; color: #333; }
+    75% { background-color: #aaa; color: #333; }
+    100% { background-color: #333; color: #efefef; }
+  }
+
+  body {
+    animation: colorShift 10s infinite;
+  }
+`;
+document.head.appendChild(style);
